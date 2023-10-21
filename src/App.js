c84a190e028d5ba76858cd58476d21135250cbe0
mugidar <mugidar@gmail.com>
2023-10-21 15:23:35 +0300

    traffic light

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const lights = ["red", "orange", "green"];
 
 function App() {
+  const [activeLight, setActiveLight] = useState("red");
+  const lightTimeInSec = 3;
+
+  useEffect(() => {
+    const nextLight =
+      lights.indexOf(activeLight) === lights.length - 1
+        ? lights[0]
+        : lights[lights.indexOf(activeLight) + 1];
+
+
+
+    const lightTimeout = setTimeout(() => {
+      setActiveLight(nextLight);
+    }, lightTimeInSec * 1000);
+
+    return () => {
+      clearTimeout(lightTimeout);
+    };
+  }, [activeLight]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="traffic_light">
+        {lights.map((color) => (
+          <span
+            key={color}
+            className={`${color} ${activeLight === color && "active"}`}
+            style={{
+              boxShadow: activeLight === color && `0 0 15px 5px ${color}`
+            }}
+          ></span>
+        ))}
+      </div>
     </div>
   );
 }
