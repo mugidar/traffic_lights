d57c80917292c38c05d0fd27fb8d09c4742862a9
mugidar <mugidar@gmail.com>
2023-10-21 15:58:40 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -3,38 +3,60 @@ import "./App.css";
 
 const lights = ["red", "orange", "green"];
 
+
 function App() {
-  const [activeLight, setActiveLight] = useState("red");
-  const lightTimeInSec = 3;
+  const [activeLight, setActiveLight] = useState("green");
+  const [lightTime, setLightTime] = useState(5);
+  const [timer, setTimer] = useState(lightTime);
 
   useEffect(() => {
-    const nextLight =
-      lights.indexOf(activeLight) === lights.length - 1
-        ? lights[0]
-        : lights[lights.indexOf(activeLight) + 1];
+    let nextLight;
+    switch (activeLight) {
+      case "red":
+        nextLight = "green";
+        break;
+      case "orange":
+        nextLight = "red";
+        break;
+      case "green":
+        nextLight = "orange";
+        break;
 
+      default:
+        break;
+    }
 
-
+    setTimer(lightTime > 0 ? lightTime : 5);
+    const interval = setInterval(() => setTimer((prev) => prev - 1), 1000);
     const lightTimeout = setTimeout(() => {
       setActiveLight(nextLight);
-    }, lightTimeInSec * 1000);
+    }, lightTime * 1000);
 
     return () => {
+      clearInterval(interval);
       clearTimeout(lightTimeout);
     };
   }, [activeLight]);
 
+
+
   return (
     <div className="App">
+      <input
+        type="number"
+        onChange={(e) => setLightTime(Number(e.target.value) > 0 ? Number(e.target.value) : 5 )}
+      />
       <div className="traffic_light">
         {lights.map((color) => (
           <span
             key={color}
-            className={`${color} ${activeLight === color && "active"}`}
+            className={`light ${color} ${activeLight === color && "active"}`}
             style={{
               boxShadow: activeLight === color && `0 0 15px 5px ${color}`
             }}
-          ></span>
+          >
+            <span className="timer">{timer}</span>
+          </span>
         ))}
       </div>
     </div>
